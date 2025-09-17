9cfb8256b445eef84c6754f5eb7fbd04b2c4cb63
ChengBlong <kallealdskogius1@gmail.com>
2025-09-17 22:36:29 +0200

    Test med ljus
    Testade lite olika saker med ljus etc i TestingScene1

---
+++ Assets/Scripts/RotatingLight.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class RotatingLight : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+
+
+}
