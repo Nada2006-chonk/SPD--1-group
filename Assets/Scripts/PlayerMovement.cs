a19faf9ce29f2a25fd225752363b9cafa442390b
ChengBlong <kallealdskogius1@gmail.com>
2025-09-19 14:51:38 +0200

    Revert "HealthBar"
    This reverts commit 42a5cf3cec39ed6a8a3e8fd988ed6d081259c1c8.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform LeftFoot, RightFoot;
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private Transform SpawnPoint;
-    [SerializeField] private HealthBar healthBar;
-
-
 
     private Rigidbody2D rgbd;
     private SpriteRenderer rend;
@@ -59,7 +56,6 @@ public class PlayerMovement : MonoBehaviour
         //när spelet startar är spelaren på full hälsa och kan röra sig (neo)
         canMove = true;
         currentHealth = startingHealth;
-        UpdateHealthBar();
 
         rgbd = GetComponent<Rigidbody2D>();
         rend = GetComponent<SpriteRenderer>();
@@ -184,9 +180,7 @@ public class PlayerMovement : MonoBehaviour
     public void TakeDamage(int damageAmount)
     {
         currentHealth -= damageAmount;
-        currentHealth = Mathf.Clamp(currentHealth, 0, startingHealth);
         print(currentHealth);
-        UpdateHealthBar();
         if (currentHealth <= 0)
         {
             Respawn();
@@ -212,13 +206,5 @@ public class PlayerMovement : MonoBehaviour
         currentHealth = startingHealth;
         transform.position = SpawnPoint.position;
         rgbd.linearVelocity = Vector2.zero;
-        UpdateHealthBar();
-    }
-
-
-    private void UpdateHealthBar()
-    {
-        float healthPercent = (float)currentHealth / startingHealth;
-        healthBar.SetHealth(healthPercent);
     }
 }
