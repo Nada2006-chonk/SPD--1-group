ca50a52916f1ab9b7c923d1ac5d1b963f5bbfe11
ChengBlong <kallealdskogius1@gmail.com>
2025-09-28 14:41:33 +0200

    level3

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,19 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset = new Vector3(0, 0, -10f);
     [SerializeField] private float smoothing = 1.0f;
+    public Vector3 Offset
+    {
+        get => offset;
+        set => offset = value;
+    }
 
     void LateUpdate()
     {
         // linear interpolation LERP, vi flyttar från en punkt till en annan över en viss tid
+
         Vector3 newPosition = Vector3.Lerp(transform.position, target.position + offset, smoothing * Time.deltaTime);
         transform.position = newPosition;
     }
+
+ 
 }
--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -7,16 +7,29 @@ public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private Transform target1, target2;
     [SerializeField] private float moveSpeed = 2.0f;
+    [SerializeField] private GameObject[] requiredEnemies;
 
     private Transform currentTarget;
+    private bool canMove;
     void Start()
     {
         currentTarget = target1;
+
+        if (requiredEnemies == null || requiredEnemies.Length == 0)
+        {
+            canMove = true;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (!canMove)
+        {
+            CheckEnemies();
+            return;
+        }
+
         if (transform.position == target1.position)
         {
             currentTarget = target2;
@@ -27,7 +40,22 @@ public class MovingPlatform : MonoBehaviour
             currentTarget = target1;
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            currentTarget.position,
+            moveSpeed * Time.deltaTime
+        );
+    }
+
+    private void CheckEnemies()
+    {
+       
+        foreach (var enemy in requiredEnemies)
+        {
+            if (enemy != null)
+                return;
+        }
+        canMove = true;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
