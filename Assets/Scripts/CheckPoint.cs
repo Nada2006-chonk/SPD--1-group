028b0dfc0cfb6b31241936c2647df894e9ff29cc
ChengBlong <kallealdskogius1@gmail.com>
2025-09-29 01:05:05 +0200

    Checkpoint prefab

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    private Animator animator;
+    private bool isActivated = false;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (!isActivated && other.CompareTag("Player"))
         {
+            isActivated = true;
             GameManager.Instance.SetCheckpoint(transform.position);
+            animator.SetTrigger("Activate");
         }
     }
 }
